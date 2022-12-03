8d5ae16df47f4157c38fd82b7c1eb61eccf74ea7
Jay <JJ.Buckby@hotmail.co.uk>
2022-12-03 01:33:24 +0000

    created return statement for app

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,14 @@ const App = () => {
   const mappedTeam = team.map((employee) => {
     return <Tracker name={employee.name} role={employee.role} />;
   });
+  return (
+    <div className="app">
+      <header className="greeting">
+        <h1 className="greeting__heading">Ticket Tracker</h1>
+        <div className="ticket-tracker">{mappedTeam}</div>
+      </header>
+    </div>
+  );
 };
 
 export default App;
