c4ef0583bba348c6e6f8eb49344b6d94d36bca74
Jay <JJ.Buckby@hotmail.co.uk>
2022-12-03 01:32:54 +0000

    created App function

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,10 @@ import "./App.scss";
 import Tracker from "./components/Tracker/Tracker";
 import team from "./assets/data/data";
 
-const App = () => {};
+const App = () => {
+  const mappedTeam = team.map((employee) => {
+    return <Tracker name={employee.name} role={employee.role} />;
+  });
+};
 
 export default App;
